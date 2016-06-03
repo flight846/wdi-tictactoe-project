8d91ab6987c08573b93937b7caa74930c01ecde4
Yazid Ismail <flight846@gmail.com>
2016-06-03 23:03:35 +0800

    Add jquery functions

--- app.js
+++ app.js
@@ -1,296 +1,86 @@
-function init()
-{
-	console.log("loaded JS");
 
-	var currentPlayer = "x";
-	var instruction;
-	var xChoices = [];
-	var oChoices =[];
-	var xPossibly = [];
-	var oPossibly = [];
-	var xDrop = 0;
-	var oDrop = 0;
-	var turns = 0;
-	var winningCom = [
-					[1,2,3], 
-					[1,4,7], 
-					[1,5,9],	
-					[2,5,8],	
-					[3,5,7],
-					[3,6,9],
-					[4,5,6],
-					[7,8,9]
-					];
+    console.clear();
 
-					
+    console.log("JS loaded");
 
-	newGame();
-
-	function runPossibly(player)
-	{
-		var numbers = [];
-		if(player == "o")
-		{
-			xPossibly.forEach(function(com)
-			{
-					numbers.push(com);	
-			});
-			
-			return numbers;
-		}
-		else if(player == "x")
-		{
-			oPossibly.forEach(function(com)
-			{		
-					numbers.push(com);
-			})
-		
-			return numbers;
-		}
-		
-	}
-
-	function checkPossiblyFor(player)
-	{		
-		if(player == "o")
-		{
-			if(oChoices.length > 0)
-			{
-				oChoices.forEach(function(choice)
-				{
-					xPossibly.forEach(function(single, j)
-					{
-						for(i=0; i < single.length; i++)
-						{
-							if(single[i] == choice)
-							{
-								xDrop++
-								delete xPossibly[j];
-								break;
-							}
-						}
-					})
-				})
-			}
-			var xPercentage = ((8-xDrop)/8)*100;
-			console.log("x :"+xPercentage);
-		}
-		else if(player == "x")
-		{
-			if(xChoices.length > 0)
-			{
-				xChoices.forEach(function(choice)
-				{
-					oPossibly.forEach(function(single, j)
-					{
-						for(i=0; i < single.length; i++)
-						{
-							if(single[i] == choice)
-							{
-								oDrop++
-								delete oPossibly[j];
-								break;
-							}
-						}
-					})
-				});
-			}
-			var oPercentage = ((8-oDrop)/8)*100;
-			console.log("o :"+oPercentage);
-		}	
-	}
-
-	function resetButton()
-	{
-		var button = document.getElementsByTagName("button");
-		button[0].addEventListener("click", newGame);		
-	}
-
-	function newGame()
-	{
-		removeAllListener();
-		addListenerToBoxes();
-		clearBoard();
-		currentPlayer = "x";
-		xChoices = [];
-		oChoices =[];
-		xDrop = 0;
-		oDrop = 0;
-		xPossibly = [
-					[1,2,3], 
-					[1,4,7], 
-					[1,5,9],	
-					[2,5,8],	
-					[3,5,7],
-					[3,6,9],
-					[4,5,6],
-					[7,8,9]
-					];
-		oPossibly = [
-					[1,2,3], 
-					[1,4,7], 
-					[1,5,9],	
-					[2,5,8],	
-					[3,5,7],
-					[3,6,9],
-					[4,5,6],
-					[7,8,9]
-					];
-		turns = 0;
-		changeInstruction("x");
-		resetButton();
-
-	}
-
-	function changeInstruction(player)
-	{
-		if(player == 'x')
-		{
-			document.querySelector("#instructions p").innerText = "It's X's turn";
-		}
-		else if (player == 'o') 
-		{
-			document.querySelector("#instructions p").innerText = "It's O's turn";
-		}	
-	}
-
-	function clearBoard()
-	{
-		var boxes = document.getElementsByClassName("box");
-		for(i=0; i < boxes.length; i++)
-		{
-			boxes[i].className = "box";
-			if(boxes[i].firstChild)
-			{
-				boxes[i].firstChild.data = "";
-			}	
-		}	
-	}
-
-	function choice(box)
-	{
-		console.log(currentPlayer);
-		if(currentPlayer == "o")
-		{
-			xChoices.push(box);
-			checkPossiblyFor("x");
-			console.log(runPossibly("x"));
-			if(checkWin(xChoices))
-			{
-				alert("We got a winner! X wins!");
-				newGame();
-			}
-		}
-		else if(currentPlayer == "x")
-		{
-			oChoices.push(box);
-			checkPossiblyFor("o");
-			console.log(runPossibly("o"));
-			if(checkWin(oChoices))
-			{
-				alert("We got a winner! O wins!");
-				newGame();
-			}
-		}
-	}
+    var grid = [null, null, null, null, null, null, null, null, null]
+    var player = 1; // =1
 
 
-	function checkWin(playerCom)
-	{
-		for(i=0; i < winningCom.length; i++)
-		{	
-			if(comparePlayerComWithSingleInt(playerCom,winningCom[i]) == true)
-			{
-				return true;
-			}
-		}
-	}
+//    function choosePlayer(input) {
+//        if (input == 'x') {
+//            return 1;
+//        } else {
+//            return 2;
+//        }
+//    }
 
-	function comparePlayerComWithSingleInt(playerChoices,singleCom)
-	{
-		var first = false;
-		var second = false;
-		var third = false;
-		singleCom.forEach(function(choice,i)
-		{
-			playerChoices.forEach(function(no)
-			{
-				if(choice == no)
-				{
-					if(i == 0 )
-					{
-						first = true;
-					}
-					else if(i == 1)
-					{
-						second = true;
-					}
-					else if(i == 2) 
-					{
-						third = true;
-					}
-					return;	
-				}
-			});
-		});
 
-		return first && second && third;
-		
-	}
+    function playTurn(index) {
+      if (grid[index] || isGameOver()) {
+        return false
+      } else {
+        grid[index] = player
+        if (player === 1) player = 2
+        else player = 1
+        return true
+      }
+    }
 
-	function listener(event)
-	{
-		turns++;
-		if(turns>8)
-		{
-			alert("Its a Draw!!");
-			newGame();
-		}
-		else
-		{
-			item = event.target.getElementsByClassName("box");
-			var nextPlayer = changePlayer();
-			event.target.className += " " +nextPlayer[0]; 
-			document.getElementById(event.toElement.id).innerText = nextPlayer[1];
-			document.getElementById(event.toElement.id).removeEventListener("click", listener);	
-			choice(event.toElement.id);
-		}
-		
-	}
 
-	function removeAllListener()
-	{
-		for(var i=1; i<10; i++)
-		{
-			document.getElementById(i).removeEventListener("click", listener);		
-		}
-	}
+    function isGameOver() {
+      if (whoWon()) {
+          return true;
+      } else {
+      return false;
+      }
+    }
 
-	function changePlayer() 
-	{
-		if(currentPlayer == "x")
-		{
-			currentPlayer = "o";
-			changeInstruction(currentPlayer);
-			return ["blue","x"];
-		}
-		else if(currentPlayer == "o")
-		{
-			currentPlayer = "x";
-			changeInstruction(currentPlayer);
-			return ["red","o"];
-		}
-		console.log(currentPlayer);
-	}	
 
-	function addListenerToBoxes()
-	{
-		for(var i=1; i<10; i++)
-		{
-			document.getElementById(i).addEventListener("click", listener);		
-		}
-	}
-	
-};
+    function whoWon() {
+      if (grid[0] && grid[0] === grid[1] && grid[0] === grid[2]) return grid[0]
+      if (grid[3] && grid[3] === grid[4] && grid[3] === grid[5]) return grid[3]
+      if (grid[6] && grid[6] === grid[7] && grid[6] === grid[8]) return grid[6]
+      if (grid[0] && grid[0] === grid[3] && grid[0] === grid[6]) return grid[0]
+      if (grid[1] && grid[1] === grid[4] && grid[1] === grid[7]) return grid[1]
+      if (grid[2] && grid[2] === grid[5] && grid[2] === grid[8]) return grid[2]
+      if (grid[0] && grid[0] === grid[4] && grid[0] === grid[8]) return grid[0]
+      if (grid[2] && grid[2] === grid[4] && grid[2] === grid[6]) return grid[2]
+      if (grid[0] && grid[1] && grid[2] && grid[3] && grid[4] &&
+        grid[5] && grid[6] && grid[7] && grid[8]) return 3
+      return 0
+    }
+    // When resert button is pressed, grid index is resetted
+    function restart() {
+      grid = [null, null, null, null, null, null, null, null, null]
+      player = 1
+    }
 
-window.addEventListener("load",init,false);
+
+$(document).ready(function() {
+    // Get html event based on the box id that we clicked and pass the id as grid index
+    $('.box').on('click', function(event) {
+        var $id=event.currentTarget.id
+        playTurn(parseInt($id));
+        console.log(grid.join("-")) //checking
+
+        //console.log(this);
+        console.log(player);
+        // Frontend- Displaying playTurn(player) to box as html
+        if (player == 1) {
+            $(this).html(2);
+        } else {
+            $(this).html(1);
+        }
+        console.log(grid);
+        console.log(whoWon());
+    })
+
+
+    $('button').on('click', function() {
+        console.log('Button clicked')
+        window.location.reload();
+    })
+
+});
+
--- tic-tac-tester.js
+++ tic-tac-tester.js
@@ -1,23 +1,16 @@
 /*global isGameOver, whoWon, playTurn, restart */
 /* Tic-Tac-Tester.js
-
 This script will test the game logic of your tic tac toe game. To use it you will need to include it in your html file after you main tic-tac-toe script. You will need to declare the following functions in the global scope:
-
 # playTurn(index)
 It should take one parameter which is a zero-based index to your grid, indicating where the current player's token should be played.
 It should return a boolean value to indicate whether the move was allowed or not - true if it was successful, false otherwise e.g. if the square is already taken or the game is over.
-
 # isGameOver()
 It should return a true or false if the game is over.
-
 # whoWon()
 It should return 0 if the game is not yet finished. Else it should return either 1 or 2 depending on which player one. It should return 3 if the game is a draw.
-
 # restart()
 It should restart the game so it can be played again.
-
 It is assumed that the turns of the player will be automatically changed after an allowed move.
-
 The application will console log all the passed or failed test */
 
 // CONFIG
@@ -56,6 +49,8 @@ console.log('==================================')
 console.log(testsRun + ' TESTS RUN')
 if (testsPassed > 0) console.log('%c>' + testsPassed + ' TESTS PASSED', 'color: green')
 if (testsFailed > 0) console.log('%c>' + testsFailed + ' TESTS FAILED', 'color: red')
+// restart the game so it can run normally
+restart()
 
 function simulateGame (testTitle, moves, winner) {
   console.log('-------------------------------')
